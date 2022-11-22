fe0dffcf23792b5d190e012a1b4e1dce35bfbc73
Shaaamila <shamila.m09@gmail.com>
2022-11-21 18:23:56 -0800

    Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
 import axios from "axios";
 
 
@@ -11,17 +12,41 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get(currentPageUrl).then(res =>{
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res =>{
+      setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p=> p.name))
+
+      return () => { cancel() }
   })
   }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading){ return "Loading..."}
 
 
 
   return (
-    <PokemonList pokemon={pokemon} />
+    <>
+      <PokemonList pokemon={pokemon} />
+
+      <Pagination 
+      gotoNextPage = {nextPageUrl ? gotoNextPage : null}
+      gotoPrevPage = {prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
   );
 }
 
